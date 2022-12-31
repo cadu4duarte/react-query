682eb94fd4e3537cdaae43cb742a27692fc64220
Carlos Eduardo <cadu4duarte@gmail.com>
2022-12-30 21:06:54 -0300

    feat: using react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
 import { useState } from 'react'
 import './App.css'
+import { useQuery } from "react-query";
 
 //reqres.in/api/users/1?delay=1
 
+export interface IData {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+async function getUser(id: number) {
+//reqres.in/api/users/1?delay=1
+  const request = await fetch(`reqres.in/api/users/${id}?delay=1`);
+
+  const response = await request.json();
+
+  if (!request.ok) {
+    throw new Error(response.error);
+  }
+
+  return response.data as IData
+  
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentId, setCurrentUserId] = useState(1);
+  const { data, isError, isLoading } = useQuery(["users", currentId], 
+    () => getUser(currentId)
+  );
+
+  if (isError) {
+    return(
+      <section>
+        <p>Somethin wen wrong</p>
+      </section>
+    );
+  }
+
+  if (!data || isLoading) {
+    return(
+      <section>
+        <p>Loading</p>
+      </section>
+    );
+  }
 
   return (
       <section>
-        <img src={""}/>
+        <img src={data.avatar}/>
 
         <p>
-          first last
+          {data.first_name} {data.last_name}
         </p>
 
-        <p>Email: email@email.com</p>
+        <p>Email: {data.email}</p>
 
         <div>
-          <button>Prev</button>
-          <button>Next</button>
+          <button onClick={() => setCurrentUserId((prev) => prev -1)}>
+            Prev
+          </button>
+          <button onClick={() => setCurrentUserId((prev) => prev +1)}>
+            Next
+          </button>
         </div>
       </section>
 
